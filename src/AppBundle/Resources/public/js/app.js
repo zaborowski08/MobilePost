60b9325139789bf6cbb840bc228f41e26035b2ef
mwaleka <walekam@gmail.com>
2016-06-11 17:01:58 +0200

    app.js fix

--- src/AppBundle/Resources/public/js/app.js
+++ src/AppBundle/Resources/public/js/app.js
@@ -14,14 +14,18 @@ var app = angular.module('postmanPanel', ['taskControllers',
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/login',{
+      templateUrl: '/bundles/app/partials/user/login-form.html',
       controller: 'LoginUserForm'
     }).
 	when('/postman/new', {
+      templateUrl: '/bundles/app/partials/postman/postman-form.html',
       controller: 'CreatePostmanForm'
   }).
 	when('/tasklist',{
+			templateUrl: '/bundles/app/partials/task-list.html',
 			controller: 'TaskListCtrl'
 	}).otherwise({
+      templateUrl: '/bundles/app/partials/index.html',
       controller: 'Dashboard'
     });;
 }]);
